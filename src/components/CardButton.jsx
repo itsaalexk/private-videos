94155e610447e632f1bc5f928e6edd2f8d22a6f1
itsaalexk <alexkononenko6@protonmail.com>
2022-11-29 19:54:02 +0100

    tried to delete videos

--- src/components/CardButton.jsx
+++ src/components/CardButton.jsx
@@ -1,13 +1,17 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-export const CardButton = ({vid , i }) => {
+export const CardButton = ({vid , i ,video}) => {
+
+  const handleDelete = ()=>{
+    video.map((v)=>{console.log(v)})
+  }
   return (
     <Card style={{ width: '420px' }}>
     <div className = "col-xxl mt-3">
        <iframe title={i} src={vid} allowFullScreen={true} width={400} height ={300}></iframe>
    </div>
-   <Button variant="outline-dark">Delete Video</Button>
+   <Button onClick = {handleDelete} variant="outline-dark">Delete Video</Button>
    </Card>
   )
 }
